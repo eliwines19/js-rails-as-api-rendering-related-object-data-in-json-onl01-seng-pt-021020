10de578d8788341e2c8818d4c56942db255ba234
Elijah wines <elijahwines84@yahoo.com>
2020-08-25 13:04:54 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,5 +1,10 @@
 class SightingsController < ApplicationController
 
+  def index
+    sightings = Sighting.all
+    render json: sightings
+  end
+
   def show
     sighting = Sighting.find_by(id: params[:id])
     render json: sighting, only: [:id, :bird_id, :location_id], include: [:bird, :location]
